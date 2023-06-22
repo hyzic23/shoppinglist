12ba0d04cccf8993beab7326508aa68155b5ad47
hyzic <isaac.oghenevwapo@gmail.com>
2023-06-22 13:26:40 -0400

    Connected to Firebase to retrieve data and display to screen

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -19,12 +19,12 @@ class _NewItemState extends State<NewItem> {
 
   final _formKey = GlobalKey<FormState>();
 
-  void _saveItem() {
+  void _saveItem() async {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
       final url = Uri.https('flutter-app-e6c30-default-rtdb.firebaseio.com',
           'shopping-list.json');
-      http.post(
+      final response = await http.post(
         url,
         headers: {'Content-Type': 'application/json'},
         body: jsonEncode(
@@ -35,6 +35,15 @@ class _NewItemState extends State<NewItem> {
           },
         ),
       );
+
+      print(response.body);
+      print(response.statusCode);
+
+      if (!context.mounted) {
+        return;
+      }
+
+      Navigator.of(context).pop();
       //   Navigator.of(context).pop(GroceryItem(
       //       id: DateTime.now().toString(),
       //       name: _enteredName,
