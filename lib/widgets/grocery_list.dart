466fa93bfe122df592f494c4cacec4533ffa6aae
hyzic <isaac.oghenevwapo@gmail.com>
2023-06-23 00:06:03 -0400

    Error handling and Added Error Message to screen

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -15,6 +15,7 @@ class GroceryList extends StatefulWidget {
 class _GroceryListState extends State<GroceryList> {
   List<GroceryItem> _groceryItems = [];
   var _isLoading = true;
+  String? _error;
 
   @override
   void initState() {
@@ -26,6 +27,13 @@ class _GroceryListState extends State<GroceryList> {
     final url = Uri.https(
         'flutter-app-e6c30-default-rtdb.firebaseio.com', 'shopping-list.json');
     final response = await http.get(url);
+
+    if (response.statusCode >= 400) {
+      setState(() {
+        _error = 'Failed to fetch data. Please try again later';
+      });
+    }
+
     final Map<String, dynamic> listData =
         jsonDecode(response.body); // Converts to map
     final List<GroceryItem> loadedItems = [];
@@ -102,6 +110,10 @@ class _GroceryListState extends State<GroceryList> {
                   ),
                 ),
               ));
+    }
+
+    if (_error != null) {
+      content = Center(child: Text(_error!));
     }
 
     return Scaffold(
